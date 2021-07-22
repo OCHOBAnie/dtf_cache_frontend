167d6c00b00355cf8b43a58a4d45cfc750ce5a42
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-07-22 19:14:16 +0700

    Header block

---
+++ src/lib/blocks/Header.svelte
@@ -0,0 +1,11 @@
+<script context="module" lang="ts">
+    import type { HeaderBlock } from '$lib/blocks/Types';
+</script>
+
+<script lang="ts">
+    export let block: HeaderBlock;
+
+    const render = () => `<${block.style} class="${block.style}">${block.text}</${block.style}>`
+</script>
+
+{@html render()}
