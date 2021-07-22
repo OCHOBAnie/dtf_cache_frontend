65250c85281dcc00ea7eed2947fec62369b1b028
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-07-22 19:14:25 +0700

    Delimeter block

---
+++ src/lib/blocks/Delimiter.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+    import type { DelimiterBlock } from '$lib/blocks/Types';
+</script>
+
+<script lang="ts">
+    export let block: DelimiterBlock;
+</script>
+
+{#if block.type === 'default'}
+    <div class="{block.type}-delimiter block h-[0.6em] text-5xl text-center tracking-widest">
+        ***
+    </div>
+{:else}
+    <div class="{block.type}">
+        *** - {block.type}
+    </div>
+{/if}
