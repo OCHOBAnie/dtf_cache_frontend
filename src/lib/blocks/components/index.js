6d75d2e299aae61f684ace7666890f58b7d6b993
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-07-22 22:28:58 +0700

    Media component

--- src/lib/blocks/components/index.js
+++ src/lib/blocks/components/index.js
@@ -2,3 +2,4 @@ export {default as Text} from './Text.svelte';
 export {default as Header} from './Header.svelte';
 export {default as Delimiter} from './Delimiter.svelte';
 export {default as Link} from './Link.svelte';
+export {default as Media} from './Media.svelte';
