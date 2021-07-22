37624cb8a1af034613f0e4652e256a35dcdf9e16
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-07-22 19:14:07 +0700

    Text block

---
+++ src/lib/blocks/Text.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+    import type { TextBlock } from '$lib/blocks/Types';
+
+    import snarkdown from 'snarkdown';
+</script>
+
+<script lang="ts">
+    export let block: TextBlock;
+</script>
+
+<p class="font-bold bg-gray-600">
+    {@html snarkdown(block.text)
+        .replace(/\\_/g, '_')
+        .replace('<a href', '<a target="_blank" href')
+    }
+</p>
