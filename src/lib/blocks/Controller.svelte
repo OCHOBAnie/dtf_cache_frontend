7730eb3327be0231473b3093620501430b7f3ef6
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-07-22 19:14:37 +0700

    Blocks controller

---
+++ src/lib/blocks/Controller.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+    import type { Block } from './Types';
+    import Text from './Text.svelte';
+    import Header from './Header.svelte';
+    import Delimiter from './Delimiter.svelte';
+
+    const options = {
+		'text': Text,
+        'header': Header,
+        'delimiter': Delimiter
+	};
+</script>
+
+<script lang="ts">
+    export let block: Block;
+</script>
+
+{#if block.type in options}
+    <svelte:component this={options[block.type]} block={block.data}/>
+{:else}
+    <span class="bg-red-600">{block.type}</span>
+{/if}
