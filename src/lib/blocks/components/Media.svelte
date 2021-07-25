a49acc3e6ea1ac9340e0b3386f23892b8134678b
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-07-25 16:18:58 +0700

    formating

--- src/lib/blocks/components/Media.svelte
+++ src/lib/blocks/components/Media.svelte
@@ -14,22 +14,21 @@
         {item.image.data.uuid}
     {/each}
     <figure>
-    {#if block.items.length === 1}
-        <div class="content-image content-image--wide"> 
-            <div class="andropov_image andropov_image--zoomable" style="max-height: {block.items[0].image.data.height}px;max-width: {block.items[0].image.data.width}px;">
-                <div class="andropov_image__inner" style="padding-bottom: 0px; background: transparent;">
-                    <img src="https://leonardo.osnova.io/{block.items[0].image.data.uuid}/-/preview/800/-/format/webp/" alt="{block.items[0].title}">
+        {#if block.items.length === 1}
+            <div class="content-image content-image--wide"> 
+                <div class="andropov_image andropov_image--zoomable" style="max-height: {block.items[0].image.data.height}px;max-width: {block.items[0].image.data.width}px;">
+                    <div class="andropov_image__inner" style="padding-bottom: 0px; background: transparent;">
+                        <img src="https://leonardo.osnova.io/{block.items[0].image.data.uuid}/-/preview/800/-/format/webp/" alt="{block.items[0].title}">
+                    </div>
                 </div>
             </div>
-        </div>
-        <figcaption class="content-image-caption l-island-a">
-            <span class="content-image-caption__title">{@html FromMD(block.items[0].title)}</span>
-            <span class="content-image-caption__author">{@html FromMD(block.items[0].author)}</span>
-        </figcaption>
-        
-    {:else}
-    <div>many</div>
-    {/if}
+            <figcaption class="content-image-caption l-island-a">
+                <span class="content-image-caption__title">{@html FromMD(block.items[0].title)}</span>
+                <span class="content-image-caption__author">{@html FromMD(block.items[0].author)}</span>
+            </figcaption>
+        {:else}
+            <div>many</div>
+        {/if}
     </figure>
 </div>
 
