5132e0f5d1a8985bbfc72e62229b70417c9f2e21
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-07-26 19:36:12 +0700

    WIP media

--- src/lib/blocks/components/Media.svelte
+++ src/lib/blocks/components/Media.svelte
@@ -7,29 +7,32 @@
     export let block: MediaBlock;
 </script>
 
-<div>
-    {block.with_background}
-    {block.with_border}
+<div class="{block.with_background ? 'l-island-b' : 'l-island-c'}">
     {#each block.items as item}
-        {item.image.data.uuid}
-    {/each}
-    <figure>
-        {#if block.items.length === 1}
-            <div class="content-image content-image--wide"> 
-                <div class="andropov_image andropov_image--zoomable" style="max-height: {block.items[0].image.data.height}px;max-width: {block.items[0].image.data.width}px;">
-                    <div class="andropov_image__inner" style="padding-bottom: 0px; background: transparent;">
-                        <img src="https://leonardo.osnova.io/{block.items[0].image.data.uuid}/-/preview/800/-/format/webp/" alt="{block.items[0].title}">
+        <figure>
+            <div class="media-single {block.with_background ? 'with_background' : ''}"> 
+                {#if item.image.data.type === "gif"}
+                    <video muted controls class="media-single__vid">
+                        <source src="https://leonardo.osnova.io/{item.image.data.uuid}/-/format/mp4/" type="video/mp4">
+                    </video>
+                {:else}
+                    <div class="media-single__image" style="max-width: {item.image.data.width}px;">
+                        <img src="https://leonardo.osnova.io/{item.image.data.uuid}/-/preview/800/-/format/webp/" alt="{item.title}">
                     </div>
-                </div>
+                {/if}
             </div>
-            <figcaption class="content-image-caption l-island-a">
-                <span class="content-image-caption__title">{@html FromMD(block.items[0].title)}</span>
-                <span class="content-image-caption__author">{@html FromMD(block.items[0].author)}</span>
-            </figcaption>
-        {:else}
-            <div>many</div>
-        {/if}
-    </figure>
+            {#if item.title || item.author}
+                <figcaption class="content-media-caption l-island-a">
+                    {#if item.title}
+                        <span class="content-media-caption__title">{@html FromMD(item.title)}</span>
+                    {/if}
+                    {#if item.author}
+                        <span class="content-media-caption__author">{@html FromMD(item.author)}</span>
+                    {/if}
+                </figcaption>
+            {/if}
+        </figure>
+    {/each}
 </div>
 
 <style>
@@ -39,12 +42,21 @@
         line-height: 1.45em;
     }
 
-    .content-image-caption__title {
+    .content-media-caption__title {
         margin-right: 15px;
     }
 
-    .content-image-caption__author {
+    .content-media-caption__author {
         font-size: 13px;
         color: #595959;
+    }
+
+    .with_background {
+        background-color: #f5fcff;
+        padding: 30px;
+    }
+
+    .with_background > div {
+        margin: 0 auto;
     }
 </style>
