5e96b22a610c4d9146beb067e750e62771af6274
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-07-26 19:35:53 +0700

    WIP new api

--- src/routes/api/[post_id].ts
+++ src/routes/api/[post_id].ts
@@ -3,7 +3,27 @@ import type { EndpointOutput, Request } from '@sveltejs/kit';
 
 export async function get({ params }: Request): Promise<EndpointOutput> {
     const { post_id } = params;
-    const res = await fetch(`https://api.dtf.ru/v1.9/entry/${post_id}`);
-    const resJson = await res.json();
-    return { body: resJson.result.blocks};
+    const res = await fetch(`https://api.vc.ru/v1.9/entry/${post_id}`);
+    let entry = await res.json();
+    entry = entry.result;
+    const authors = [{
+        type: 'user',
+        url: entry.author.url,
+        name: entry.author.name,
+        avatar_url: entry.author.avatar_url
+    }];
+    if (entry.author.url !== entry.subsite.url) {
+        authors.push({
+            type: 'subsite',
+            url: entry.subsite.url,
+            name: entry.subsite.name,
+            avatar_url: entry.subsite.avatar_url
+        })
+    }
+    const entryData = {
+        url: entry.url,
+        authors: authors,
+        date: entry.date
+    }
+    return { body: { entryData, entryBlocks: entry.blocks } };
 }
